1c5e0de32e53261ca407bb2fbbb92ffe268150d1
RichardBray <richardbray89@gmail.com>
2024-04-15 10:52:51 +0100

    chore: add code for zap

--- logging.go
+++ logging.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"os"
-	"time"
+	"log"
 
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	logger.Info("this is an info message", zap.String("username", "johndoe"))
+	// logger.Info("this is an info message", "username", "johndoe")
+	sugar := logger.Sugar()
+	defer logger.Sync()
 
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	logger := zerolog.New(output).With().Timestamp().Caller().Logger()
-
-	log.Trace().Msg("this is a debug message")
-	logger.Debug().Msg("this is a debug message")
-	logger.Info().Msg("this is an info message")
-	logger.Warn().Msg("this is a warn message")
-	logger.Error().Msg("this is an error message")
-	// log.Fatal().Msg("this is a fatal message")
-	// log.Panic().Msg("This is a panic message")
+	sugar.Debug("this is a debug message")
+	sugar.Infof("this is an info message", 1234, "johndoe")
+	sugar.Info("this is an info message")
+	sugar.Warn("this is a warn message")
+	// sugar.Error("this is an error message")
+	// sugar.Fatal("this is a fatal message")
 }
