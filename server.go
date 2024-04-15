c804bfa1fcbb888cb930394070318f2885d09a40
RichardBray <richardbray89@gmail.com>
2024-04-15 11:54:01 +0100

    feat: add server binary

--- server.go
+++ server.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"log"
+	"log/slog"
+	"os"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
 
+	logHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true,
+	})
+
+	logger := slog.New(logHandler)
+
+	slog.SetDefault(logger)
+
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		log.Println("Hello, World!")
+		slog.Info("Hello, World!")
 		return c.SendString("Hello, World!")
 	})
 
 	app.Listen(":3000")
-	log.Println("Server started on port 3000")
+	slog.Info("Server started on port 3000")
 }
