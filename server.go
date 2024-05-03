8a2e4aa0896edc607ce045e76a61cc9e0abb86cc
Richard Oliver Bray <richardbray89@gmail.com>
2024-05-03 16:20:25 +0100

    chore: update print text

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		fmt.Println("Hello, World!")
+		fmt.Println("Hello, World Again!")
 		return c.SendString("Hello, World!")
 	})
 
