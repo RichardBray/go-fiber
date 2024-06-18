4c28332c03b4c5b64954fd573538e4e05b581731
RichardBray <richardbray89@gmail.com>
2024-06-18 16:52:17 +0100

    chore: updated message to german

--- server.go
+++ server.go
@@ -27,7 +27,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello World")
+		return c.SendString("Hallo Welt")
 	})
 
 	app.Get("/articles", func(c fiber.Ctx) error {
