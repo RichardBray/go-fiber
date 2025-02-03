8c19f9c01008a485343b4c9351df0c05393560bb
RichardBray <richardbray89@gmail.com>
2025-02-03 21:36:27 +0100

    fix: send logs to otel without fiber

--- logger.go
+++ logger.go
@@ -6,31 +6,50 @@ import (
 
 	"go.opentelemetry.io/contrib/bridges/otelslog"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
-	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
-func CreateLogger() *slog.Logger {
+func main() {
 	// What does this do?
 	ctx := context.Background()
 
-	logExporter, err := otlploghttp.New(ctx)
+	res, err := newResource()
+
+	logExporter, err := otlploghttp.New(ctx, otlploghttp.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
 
 	logProvider := log.NewLoggerProvider(
+		log.WithResource(res),
 		log.WithProcessor(log.NewBatchProcessor(logExporter)),
 	)
 
 	// makes sure resources are cleaned up when logProvider is not needed anymore
 	defer logProvider.Shutdown(ctx)
 
-	global.SetLoggerProvider(logProvider)
-
 	logger := otelslog.NewLogger("hello",
 		otelslog.WithSource(true),
+		otelslog.WithLoggerProvider(logProvider),
 	)
 
-	return logger
+	logger.Debug("Are you alive?")
+
+	logger.Info("This is an info log message.")
+	logger.Error("This is an error log message.")
+
+	// Optionally, you can also log with additional context
+	logger.Info("Log with context", slog.String("context.key", "context value"))
+
+	// return logger
+}
+
+func newResource() (*resource.Resource, error) {
+	return resource.Merge(resource.Default(),
+		resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceName("my-service"),
+			semconv.ServiceVersion("0.1.0"),
+		))
 }
--- main.go
+++ main.go
@@ -1,41 +1,41 @@
-package main
+// package main
 
-import (
-	"github.com/gofiber/fiber/v3"
-)
+// import (
+// 	"github.com/gofiber/fiber/v3"
+// )
 
-type Article struct {
-	Title string
-	Text  string
-}
+// type Article struct {
+// 	Title string
+// 	Text  string
+// }
 
-func main() {
-	articles := []Article{}
+// func main() {
+// 	articles := []Article{}
 
-	logger := CreateLogger()
+// 	logger := CreateLogger()
 
-	app := fiber.New()
+// 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hallo Welt!!")
-	})
+// 	app.Get("/", func(c fiber.Ctx) error {
+// 		return c.SendString("Hallo Welt!!")
+// 	})
 
-	app.Get("/articles", func(c fiber.Ctx) error {
-		logger.Info("Getting all articles")
-		return c.JSON(articles)
-	})
+// 	app.Get("/articles", func(c fiber.Ctx) error {
+// 		logger.Info("Getting all articles")
+// 		return c.JSON(articles)
+// 	})
 
-	app.Post("/article", func(c fiber.Ctx) error {
-		article := new(Article)
-		if err := c.Bind().Body(article); err != nil {
-			logger.Error("Error parsing article", err)
-			return err
-		}
-		articles = append(articles, *article)
-		logger.Info("Added new article")
-		return c.JSON(article)
-	})
+// 	app.Post("/article", func(c fiber.Ctx) error {
+// 		article := new(Article)
+// 		if err := c.Bind().Body(article); err != nil {
+// 			logger.Error("Error parsing article", err)
+// 			return err
+// 		}
+// 		articles = append(articles, *article)
+// 		logger.Info("Added new article")
+// 		return c.JSON(article)
+// 	})
 
-	PORT := ":3000"
-	app.Listen(PORT)
-}
+// 	PORT := ":3000"
+// 	app.Listen(PORT)
+// }
