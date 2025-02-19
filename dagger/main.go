bd8a7c7e20274fc5a606158ba876cbeddb468e23
RichardBray <richardbray89@gmail.com>
2025-02-19 17:57:01 +0000

    chore: create extra function for reusability

--- dagger/main.go
+++ dagger/main.go
@@ -24,15 +24,22 @@ func (m *GoFiber) GrepDir(ctx context.Context, directoryArg *dagger.Directory, p
 
 // Run application unit tests
 func (m *GoFiber) Test(ctx context.Context, source *dagger.Directory) (string, error) {
+	return m.BaseEnv(source).
+		WithExec([]string{"go", "test", "-v"}).
+		Stdout(ctx)
+}
+
+// Sets up the base Go environment
+func (m *GoFiber) BaseEnv(source *dagger.Directory) *dagger.Container {
 	return dag.Container().
 		From("golang:1.23").
 		WithMountedDirectory("/src", source).
 		WithWorkdir("/src").
 		// WithExec([]string{"go", "mod", "download"}).
-		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-123")).
-		WithExec([]string{"go", "test", "-v"}).
-		Stdout(ctx)
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod-123"))
 }
+
+// ... existing code ...
 
 // func (m *GoFiber) Test(ctx context.Context, source *dagger.Directory) (string, error) {
 // 	return dag.Go().
