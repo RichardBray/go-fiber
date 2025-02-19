2c51df5c9a2ce1d9d0fc46af600bb74f0468db9a
RichardBray <richardbray89@gmail.com>
2025-02-19 10:50:39 +0000

    chore: remove unecessary files

--- main.go
+++ main.go
@@ -12,8 +12,6 @@ type Article struct {
 func main() {
 	articles := []Article{}
 
-	logger := CreateLogger()
-
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
@@ -21,18 +19,15 @@ func main() {
 	})
 
 	app.Get("/articles", func(c fiber.Ctx) error {
-		logger.Info("Getting all articles")
 		return c.JSON(articles)
 	})
 
 	app.Post("/article", func(c fiber.Ctx) error {
 		article := new(Article)
 		if err := c.Bind().Body(article); err != nil {
-			logger.Error("Error parsing article", err)
 			return err
 		}
 		articles = append(articles, *article)
-		logger.Info("Added new article")
 		return c.JSON(article)
 	})
 
